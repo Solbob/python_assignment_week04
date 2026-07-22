dfb20cdafe277c906c27389f985dd07367e74793
Solbob <85553606+Solbob@users.noreply.github.com>
2026-07-22 03:40:50 +0000

    finish calculator and lambda

---
+++ lambda_calculator.py
@@ -0,0 +1,10 @@
+add = lambda a, b: a + b
+subtract = lambda a, b: a - b
+multiply = lambda a, b: a * b
+divide = lambda a, b: a / b
+
+if __name__ == '__main__':
+    print('add(5, 3) =', add(5, 3))
+    print('subtract(10, 4) =', subtract(10, 4))
+    print('multiply(7, 2) =', multiply(7, 2))
+    print('divide(20, 5) =', divide(20, 5))
